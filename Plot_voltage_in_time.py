b04499dd1b35bb76b6c35f71e154ef55cd22bc61
Valentin <valentin.druelle@unibas.ch>
2022-06-10 15:53:16 +0200

    Plot voltage in time

--- Plot_voltage_in_time.py
+++ Plot_voltage_in_time.py
@@ -1,57 +1,60 @@
 """
-================================================
-ABElectronics ADC Pi 8-Channel ADC demo
-Requires python smbus to be installed
-run with: python demo_readvoltage.py
-================================================
-Initialise the ADC device using the default addresses and sample rate,
-change this value if you have changed the address selection jumpers
-Sample rate can be 12,14, 16 or 18
+Plot a graphic of the voltage read in time
 """
 
 from __future__ import absolute_import, division, print_function, \
-                                                    unicode_literals
+    unicode_literals
 import time
 import os
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from ADCPi import ADCPi
 
-try:
-    from ADCPi import ADCPi
-except ImportError:
-    print("Failed to import ADCPi from python system path")
-    print("Importing from parent folder instead")
-    try:
-        import sys
-        sys.path.append('..')
-        from ADCPi import ADCPi
-    except ImportError:
-        raise ImportError(
-            "Failed to import library from parent folder")
+# try:
+#     from ADCPi import ADCPi
+# except ImportError:
+#     print("Failed to import ADCPi from python system path")
+#     print("Importing from parent folder instead")
+#     try:
+#         import sys
+#         sys.path.append('..')
+#         from ADCPi import ADCPi
+#     except ImportError:
+#         raise ImportError(
+#             "Failed to import library from parent folder")
 
 
-def main():
-    '''
-    Main program function
-    '''
+def animate(i, xs, ys, adc, io_channel, nb_data=50):
+    "Plot the voltage read over time."
 
-    adc = ADCPi(0x68, 0x69, 12)
+    # Read the data
+    voltage = adc.read_voltage(io_channel)
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(voltage)
 
-    while True:
+    xs = xs[-nb_data:]
+    ys = ys[-nb_data:]
 
-        # clear the console
-        os.system('clear')
+    # Plot the data
+    ax.clear()
+    ax.plot(xs, ys)
+    ax.plot([xs[0], xs[-1]], [np.mean(ys), np.mean(ys)], "r-")
 
-        # read from adc channels and print to screen
-        print("Channel 1: %02f" % adc.read_voltage(1))
-        print("Channel 2: %02f" % adc.read_voltage(2))
-        print("Channel 3: %02f" % adc.read_voltage(3))
-        print("Channel 4: %02f" % adc.read_voltage(4))
-        print("Channel 5: %02f" % adc.read_voltage(5))
-        print("Channel 6: %02f" % adc.read_voltage(6))
-        print("Channel 7: %02f" % adc.read_voltage(7))
-        print("Channel 8: %02f" % adc.read_voltage(8))
+    # Format the plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Voltage over time')
+    plt.ylabel('Voltage')
 
-        # wait 0.2 seconds before reading the pins again
-        time.sleep(0.2)
 
 if __name__ == "__main__":
-    main()
+    adc = ADCPi(0x68, 0x69, 12)
+    io_channel = 8
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    xs = []
+    ys = []
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, adc, io_channel), interval=200)
+    plt.show()
